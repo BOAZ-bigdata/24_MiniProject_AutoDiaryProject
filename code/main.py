3458679f5891ca4370531b72162d29aee83a077e
juhyun <peter520416@gmail.com>
2025-02-25 16:34:43 +0000

    main.py 파일 추가

---
+++ code/main.py
@@ -0,0 +1,15 @@
+import subprocess
+
+def main():
+    try:
+        # Streamlit 앱 실행
+        print("Streamlit 앱을 실행하는 중...")
+        subprocess.run(["streamlit", "run", "code/blip_streamlit.py"], check=True)
+        
+    except subprocess.CalledProcessError as e:
+        print(f"오류 발생: {e}")
+    except Exception as e:
+        print(f"예상치 못한 오류 발생: {e}")
+
+if __name__ == "__main__":
+    main()
